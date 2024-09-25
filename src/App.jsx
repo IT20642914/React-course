79ec03cd22bacb22a4561522e7de3a45c4126446
IT20642914 <avishkachanaka@gmail.com>
2024-09-25 18:26:30 +0530

    Add Vite configuration and React setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import {CORE_CONCEPTS} from './data';
+import CoreConcepts from './components/CoreConcepts/CoreConcepts';
+import Header from './components/Header/Header';
+
+
+
+
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+    <Header/> 
       <main>
-        <h2>Time to get started!</h2>
+     <section id='core-concepts'>
+      <h2>Core concepts </h2>
+      <ul>
+      {CORE_CONCEPTS && CORE_CONCEPTS.map((concept,index)=>(
+          <CoreConcepts {...concept} key={index}/>
+      ))}
+    
+      </ul>
+
+     </section>
       </main>
     </div>
   );
