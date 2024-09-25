466eb686e1066de2c8c17cd32210b9262a23490b
IT20642914 <avishkachanaka@gmail.com>
2024-09-25 19:18:05 +0530

    Refactor App component and add TabButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import {CORE_CONCEPTS} from './data';
 import CoreConcepts from './components/CoreConcepts/CoreConcepts';
 import Header from './components/Header/Header';
-
-
-
-
-
+import TabButton from './components/TabButton/TabButton';
 function App() {
+  const handleSelect = (selectedBtn) => {
+    console.log('clicked',selectedBtn)
+        }
   return (
     <div>
     <Header/> 
@@ -17,10 +16,19 @@ function App() {
       {CORE_CONCEPTS && CORE_CONCEPTS.map((concept,index)=>(
           <CoreConcepts {...concept} key={index}/>
       ))}
-    
       </ul>
 
      </section>
+     <section id='examples'>
+      <h2>Examples </h2>
+      <menu>
+      <TabButton onClick={()=>handleSelect("Components")}>Components</TabButton>
+      <TabButton onClick={()=>handleSelect("JSX")}>JSX</TabButton>
+      <TabButton onClick={()=>handleSelect("Props")}>Props</TabButton>
+      <TabButton onClick={()=>handleSelect("Props")}>Props</TabButton>
+      </menu>
+
+      </section>
       </main>
     </div>
   );
---
+++ src/components/TabButton/TabButton.jsx
@@ -0,0 +1,11 @@
+
+const TabButton = ({children,onClick}) => {
+
+  return (
+   <li>
+    <button onClick={onClick}>{children}</button>
+   </li>
+  )
+}
+
+export default TabButton
